b5f0b0c36a34f6b7e2a324398c240816593aa8f3
Mihlali-Ma <129970639+Mihlali-Ma@users.noreply.github.com>
2023-05-16 14:21:12 +0200

    hey

---
+++ script/code.js
@@ -0,0 +1,9 @@
+let subjects = ['Python', 'C++', 'VB.Net', 'Javascript','Assembly'];
+
+//lastsubjects = subjects.pop()
+console.log(subjects.pop());
+subjects.pop()
+console.log(subjects.pop());
+
+
+  
